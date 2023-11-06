6f931d0ac8635825eb7f102be221e1c89e990a44
chriss1525 <okothchris15@gmail.com>
2023-11-06 20:04:10 +0300

    query the star wars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,23 @@
+//script that prints all the characters of a Star Wars movie:
+
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(url + process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body).characters;
+
+    for (const i in data) {
+      request(data[i], (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,3 @@
+# Star Wars API
+
+Querying the Starwars API and listing the names of characters per movie.
