cea7765ba5fc80d6f952bdcbcf7ae2e5f1ac3ad9
chriss1525 <okothchris15@gmail.com>
2023-11-10 09:26:08 +0300

    use request to list all characters in the star wars movies

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,52 @@
 #!/usr/bin/node
-//script that prints all the characters of a Star Wars movie:
+// script that prints all the characters of a Star Wars movie:
 
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/films/';
+const getCharacterName = (characterUrl) => {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+};
 
-request(url + process.argv[2], (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body).characters;
+const getMovieCharacters = (movieId) => {
+  return new Promise((resolve, reject) => {
+    const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-    for (const i in data) {
-      request(data[i], (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const characters = JSON.parse(body).characters;
+        resolve(characters);
+      }
+    });
+  });
+};
+
+const printCharacters = async (movieId) => {
+  try {
+    const characters = await getMovieCharacters(movieId);
+
+    for (const character of characters) {
+      const characterName = await getCharacterName(character);
+      console.log(characterName);
     }
+  } catch (error) {
+    console.error(error);
   }
-});
+};
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Please provide a Movie ID as a command line argument.');
+} else {
+  printCharacters(movieId);
+}
