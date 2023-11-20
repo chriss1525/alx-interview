2d8afa070cfb58c667accf5b161b4aba3ca1afed
chriss1525 <okothchris15@gmail.com>
2023-11-21 01:37:10 +0300

    read sdin line by line and computes the metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,41 +3,59 @@
 
 """ script that reads stdin line by line and computes metrics"""
 import sys
-import re
-from collections import defaultdict
+import signal
 
-# Define a dictionary to store status code counts
-status_code_counts = defaultdict(int)
+# Dictionary to store file sizes and status codes
+file_sizes = {}
+status_codes = {200, 301, 400, 401, 403, 404, 405, 500}
 
-# Initialize total file size
+# Variables to keep track of total file size and line count
 total_size = 0
+line_count = 0
+
+# Function to print statistics
+
+
+def print_statistics():
+    print(f'Total file size: File size: {total_size}')
+    for code in sorted(status_codes):
+        if code in file_sizes:
+            print(f'{code}: {file_sizes[code]}')
+
+# Function to handle keyboard interruption (CTRL + C)
+
+
+def signal_handler(sig, frame):
+    print_statistics()
+    sys.exit(0)
+
+
+# Register the signal handler for CTRL + C
+signal.signal(signal.SIGINT, signal_handler)
 
 try:
     for line in sys.stdin:
-        # Extract relevant data using regex
-        match = re.match(
-            r'(\d+\.\d+\.\d+\.\d+) - \[.*\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)', 
-            line)
-        if match:
-            ip, status_code, file_size = match.groups()
-            # Increment status code count
-            status_code_counts[int(status_code)] += 1
-            # Update total file size
-            total_size += int(file_size)
+        # Parse the line using a simple approach
+        parts = line.split()
+        if len(parts) >= 10 and parts[8].isdigit():
+            status_code = int(parts[8])
+            file_size = int(parts[9])
 
-        # Check if 10 lines have been processed, and if so, print the statistics
-        if len(status_code_counts) >= 8:
-            print(f'Total file size: File size: {total_size}')
-            for code in sorted(status_code_counts.keys()):
-                print(f'{code}: {status_code_counts[code]}')
-            print('---')
-            # Reset the status code counts and total size
-            status_code_counts = defaultdict(int)
-            total_size = 0
+            # Update total file size
+            total_size += file_size
+
+            # Update status code count
+            if status_code in status_codes:
+                file_sizes[status_code] = file_sizes.get(status_code, 0) + 1
+
+            # Update line count
+            line_count += 1
+
+            # Print statistics every 10 lines
+            if line_count % 10 == 0:
+                print_statistics()
 
 except KeyboardInterrupt:
-    # Handle Ctrl+C gracefully and print the current statistics
-    print(f'Total file size: File size: {total_size}')
-    for code in sorted(status_code_counts.keys()):
-        print(f'{code}: {status_code_counts[code]}')
-
+    # Handle keyboard interruption (CTRL + C)
+    print_statistics()
+    sys.exit(0)
