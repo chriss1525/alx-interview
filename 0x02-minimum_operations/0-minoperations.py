7b20fd989022b2f607432e4b47979b719dfa6449
chriss1525 <okothchris15@gmail.com>
2023-11-21 01:29:07 +0300

    determine the minimum number of operations needed to result in exactly n H characters in a file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+""" number that calculates the fewest number of operations needed to result"""
+
+def minOperations(n):
+    """ minOperations function """
+    if n <= 1:
+        return 0
+    else:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                return minOperations(int(n / i)) + i
+
+    return 0
