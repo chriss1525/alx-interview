91a33bb47e1bf01cc064dde5c8871eb8bb76db30
chriss1525 <okothchris15@gmail.com>
2023-12-05 13:14:55 +0300

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Prime Game"""
+
+def isWinner(x, nums):
+    """Prime Game"""
+    if not nums or x < 1:
+        return None
+    n = max(nums)
+    sieve = [True] * (n + 1)
+    sieve[0] = False
+    sieve[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+    sieve = [i for i in range(len(sieve)) if sieve[i]]
+    c = 0
+    for i in range(x):
+        for j in sieve:
+            if j <= nums[i]:
+                c += 1
+        if c % 2 == 0:
+            return "Ben"
+        else:
+            return "Maria"
